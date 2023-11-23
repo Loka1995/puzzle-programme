a0ea420008b3c3e458eb0713d6fc43b0b2815e45
Kalana Ekanayake <ekanayakekalana12@gmail.com>
2023-11-23 10:50:49 +0530

    First checkpoint LOLgit add .!

--- test.js
+++ test.js
@@ -1,5 +1,22 @@
-import { HomeView } from "./views/HomeView.js";
+const createPuzzle = (puzzlePieces, puzzleSize) => {
+    for (let i = 1; i <= puzzlePieces; i++) {
+        const template = document.createElement('template');
+        template.innerHTML =
+            `
+                <div class="grid-item dropzone" id="${i}div" attachedImg="${i}img">
+                    <img
+                        src="images/${puzzleSize}/image${i}.png"
+                        alt="${i}st piece"
+                        id="${i}img" />
+                </div>
+            `;
 
-const homeView = new HomeView();
-const body = document.getElementsByTagName('body')[0];
-body.appendChild(homeView)
+        const templateContent = template.content;
+        const container = document.getElementById('grid-container');
+        container.appendChild(templateContent);
+    }
+}
+
+createPuzzle(9,'3x3');
+
+
